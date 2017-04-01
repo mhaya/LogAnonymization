663e6973d82bfa967053209af6089dd9d9444c70
mhaya <masaharu.hayashi3@gmail.com>
2017-04-01 09:29:12 +0900

    add cryptopan2.py

---
+++ CryptoPAn/cryptopan2.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import Crypto.Cipher.AES
+import socket
+import struct
+import array
+import ipaddress
+import numpy as np
+
+#Orig IP: 192.168.0.1
+#Ano IP: 2.149.253.242
+  
+class CryptoPAn:
+    def __init__(self,key):
+        self._init(key)
+
+    def _init(self,key):
+        self._cipher = Crypto.Cipher.AES.new(key[:16],Crypto.Cipher.AES.MODE_ECB)
+        padding = array.array('B')
+        padding.fromstring(self._cipher.encrypt(key[16:]))
+        self._padding_int = self._to_int(padding)
+        
+    def changeKey(slef,key):
+        self._init(key)
+    
+    def MSB(self,byte_array):
+        n = byte_array[0] >> 7
+        return n
+    
+    def LSB(self,byte_array):
+        n = self._to_int(byte_array)
+        return n & 1
+
+    def anonymize(self,addr,version,priv):
+        if version == 4:
+            N = 32
+            ext_addr = addr << 96
+        elif version == 6:
+            N = 128
+            ext_addr = addr 
+         
+        flip_array = []
+        
+        for pos in range(N):
+            prefix =  ext_addr >> (128-pos) << (128-pos) 
+            padded_addr = prefix | (self._padding_int & (2**128-1 >> pos))
+            self._input = self._to_byte_array(padded_addr,16)
+            output = array.array('B')
+            output.fromstring(self._cipher.encrypt(self._input))
+            # 論文だとLSBだけど実装はMSB
+            flip_array.append(self.MSB(output))
+        result = reduce(lambda x, y: (x << 1) | y, flip_array)
+        
+        anonymizedIP = addr ^ (result & (2**N-1<<(N-priv)))
+        return anonymizedIP
+    
+    def _to_int(self,byte_array):
+        return reduce(lambda x,y: x << 8 | y,byte_array) 
+    
+    def _to_byte_array(self,n,byte_cnt):
+        byte_array = array.array('B')
+        for i in range(byte_cnt):
+            byte_array.insert(0, (n >> (i * 8)) & 0xff)
+        return byte_array
+    
+if __name__ == '__main__' :
+    # 192.0.2.1 > 2.90.93.17
+    # 2001:db8::1 > dd92:2c44:3fc0:ff1e:7ff9:c7f0:8180:7e00
+    key = array.array('B',range(32))
+    obj = CryptoPAn(key)
+    orig_addr = '192.0.2.1'
+    if not isinstance(orig_addr,unicode):
+        orig_addr = unicode(orig_addr)
+
+    ip=ipaddress.ip_address(orig_addr)
+    anonymizedIP = obj.anonymize(int(ip),4,32)
+    anonymizedIP = ipaddress.IPv4Address(anonymizedIP)
+    print "Orig IP:",ip
+    print "Ano IP(2.90.93.17):",anonymizedIP 
+    orig_addr = '192.0.2.2'
+    if not isinstance(orig_addr,unicode):
+        orig_addr = unicode(orig_addr)
+
+    ip=ipaddress.ip_address(orig_addr)
+    anonymizedIP = obj.anonymize(int(ip),4,32)
+    
+    anonymizedIP = ipaddress.IPv4Address(anonymizedIP)
+    print "Orig IP:",ip
+    print "Ano IP(2.90.93.19):",anonymizedIP 
+    orig_addr = '2001:db8::1'
+    if not isinstance(orig_addr,unicode):
+        orig_addr = unicode(orig_addr)
+
+    ip=ipaddress.ip_address(orig_addr)
+    anonymizedIP = obj.anonymize(int(ip),6,128)
+    anonymizedIP = ipaddress.IPv6Address(anonymizedIP)
+    print "Orig IP:",ip
+    print "Ano IP(dd92:2c44:3fc0:ff1e:7ff9:c7f0:8180:7e00):",anonymizedIP 
+    orig_addr = '2001:db8::2'
+    if not isinstance(orig_addr,unicode):
+        orig_addr = unicode(orig_addr)
+
+    ip=ipaddress.ip_address(orig_addr)
+    anonymizedIP = obj.anonymize(int(ip),6,128)
+    anonymizedIP = ipaddress.IPv6Address(anonymizedIP)
+    print "Orig IP:",ip
+    print "Ano IP(dd92:2c44:3fc0:ff1e:7ff9:c7f0:8180:7e02):",anonymizedIP   
+
+        
+
---
+++ CryptoPAn/usecryptopan2.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append(".")
+import array
+import cryptopan2
+
+argvs = sys.argv
+ 
+f = open(argvs[1])
+line = f.readline() 
+
+key = array.array('B',range(32))
+obj = cryptopan2.CryptoPAn(key)
+
+while line:
+    tmp = line.split(",")
+    tmp[1] = str(obj.anonymize(int(tmp[1]),4,32))
+    tmp[2] = str(obj.anonymize(int(tmp[2]),4,32))
+    print(tmp[0]+","+tmp[1]+","+tmp[2]+","+tmp[3]+","+tmp[4]+","+tmp[5].strip())
+    line = f.readline() 
+    
+f.close()
+
